ef50f2ca9cb8a7d655f8d0c460b41f52b87e2704
Konstantin <Steelmen87@bk.ru>
2022-04-09 18:27:03 +0300

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,76 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = "hidden-detail";
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
+var ESC_KEY = 27;
+var TINY_EFFECT_CLASS = "is-tiny";
 
 function setDetails(imageUrl, titleText) {
-    'use strict';
+    "use strict";
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
     var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
-    detailImage.setAttribute('src', imageUrl);
+    detailImage.setAttribute("src", imageUrl);
     detailTitle.textContent = titleText;
 }
 
 function imageFromThumb(thumbnail) {
-    'use strict';
-    return thumbnail.getAttribute('data-image-url')
+    "use strict";
+    return thumbnail.getAttribute("data-image-url");
 }
 
 function titleFromThumb(thumbnail) {
-    'use strict';
-    return thumbnail.getAttribute('data-image-title')
+    "use strict";
+    return thumbnail.getAttribute("data-image-title");
 }
 
 function setDetailFromThumb(thumbnail) {
-    'use strict';
-    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail))
+    "use strict";
+    setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
 function addThumbClickHandler(thumb) {
-    'use stict'
-    thumb.addEventListener('click', function(event) {
+    "use stict";
+    thumb.addEventListener("click", function(event) {
         event.preventDefault();
-        setDetailFromThumb(thumb)
-    })
+        setDetailFromThumb(thumb);
+        showDetails();
+    });
 }
 
 function getThumbnailsArray() {
-    'use strict';
+    "use strict";
     var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
     var thumbnailArray = [].slice.call(thumbnails);
     return thumbnailArray;
 }
 
+function hideDetails() {
+    document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+    var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+    document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+    frame.classList.add(TINY_EFFECT_CLASS);
+    setTimeout(() => {
+        frame.classList.remove(TINY_EFFECT_CLASS);
+    }, 50);
+}
+
+function addKeyPressHandler() {
+    document.body.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === ESC_KEY) {
+            hideDetails();
+        }
+    });
+}
+
 function initializeEvents() {
-    'use strict';
+    "use strict";
     var thumbnail = getThumbnailsArray();
-    thumbnail.forEach(addThumbClickHandler)
+    thumbnail.forEach(addThumbClickHandler);
+    addKeyPressHandler();
 }
 initializeEvents();
